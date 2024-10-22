b8aa6e495f5d389e67e8b9bfcffb01cdbfad5750
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-10-22 21:07:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from "./Component/Button.jsx";
+import "./index.css";
+import Header from "./Component/Header.jsx";
+import Hero from "./Component/Hero.jsx";
+import Benefits from "./Component/Benefits.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = ({ Heading, className }) => {
+  return <div>{title && <h2 className="h2">{title}</h2>}</div>;
+};
+
+export default Heading;
